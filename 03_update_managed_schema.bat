3c53a75cfa8feb6a787f35af54bd6376aed73392
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2021-10-29 19:11:47 +0300

    name of collection shaqodoon
    working on issue #11

--- 03_update_managed_schema.bat
+++ 03_update_managed_schema.bat
@@ -1,2 +1,2 @@
 @echo off
-xcopy mycollection_schema\managed-schema c:\Solr\solr-8.10.1\server\solr\mycollection\conf\*.* /y
+xcopy mycollection_schema\managed-schema c:\Solr\solr-8.10.1\server\solr\shaqodoon\conf\*.* /y
